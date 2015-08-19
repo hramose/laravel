d7ff4ddf204897a9e9a6517494726fca6e5284a4
Vinayak <vinayak.kulkarni@live.com>
2015-08-20 01:21:09 +0530

    updated Routes for Auth & Registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,8 +26,17 @@ Route::post('about','PagesController@store');
 //Route::post('articles','ArticlesController@store');
 
 Route::resource('articles','ArticlesController');
-
+/*
 Route::controllers([
     'auth' => 'Auth\AuthController',
     'password' => 'Auth\PasswordController',
-]);
+]);*/
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
